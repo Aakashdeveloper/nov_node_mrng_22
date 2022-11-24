fe11e25dde3522403fe5b47bcd9824f6a158990a
aakashdeveloper <ahanda205@gmail.com>
2022-11-24 08:56:10 +0530

    products

--- fullstack/src/controller/productRouter.js
+++ fullstack/src/controller/productRouter.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let productRouter = express.Router();
-const mongodb = require('mongodb').MongoClient;
+let mongo = require('mongodb');
+const mongodb = mongo.MongoClient;
 const url = process.env.MongoUrl;
 
 
@@ -28,14 +29,43 @@ function router(menu){
 
     productRouter.route('/category/:id')
         .get(function(req,res){
-            const id = req.params.id
-            res.send(id)
+            //const id = req.params.id
+            let {id} = req.params
+            mongodb.connect(url,function(err,dc){
+                if(err){
+                    res.status(500).send('Error While Connecting')
+                }else{
+                   let dbObj = dc.db('aug9');
+                   dbObj.collection('products').find({'category_id':Number(id)}).toArray(function(err,proddata){
+                       if(err){
+                           res.status(400).send('Error While Fetching')
+                       }else{
+                           res.status(200).render('product',{title:'Products',proddata,menu})
+                       }
+                   })
+                }
+            })
         })
 
     //detail route of products
-    productRouter.route('/details')
+    productRouter.route('/details/:id')
         .get((req,res) => {
-            res.send('Products Details')
+            let id = mongo.ObjectId(req.params.id)
+            mongodb.connect(url,function(err,dc){
+                if(err){
+                    res.status(500).send('Error While Connecting')
+                }else{
+                   let dbObj = dc.db('aug9');
+                   dbObj.collection('products').find({'_id':id}).toArray(function(err,data){
+                       console.log(data)
+                       if(err){
+                           res.status(400).send('Error While Fetching')
+                       }else{
+                           res.status(200).render('productDetails',{data,menu})
+                       }
+                   })
+                }
+            })
         })
 
     return productRouter
