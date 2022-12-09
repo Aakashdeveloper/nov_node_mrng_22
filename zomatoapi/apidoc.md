0ecab9a9bcb6bb429ed0e4caee30021e7c750afd
aakashdeveloper <ahanda205@gmail.com>
2022-12-09 08:52:35 +0530

    restapi

--- zomatoapi/apidoc.md
+++ zomatoapi/apidoc.md
@@ -1,18 +1,23 @@
 ///Page1
 * List of all cities
-> http://localhost:9100/location
+> http://localhost:2400/location
 
 * List of all restaurant
-> http://localhost:9100/restaurants
+> http://localhost:2400/restaurants
 
 * Restaurants wrt City
+> http://localhost:2400/restaurants?stateId=2
+> http://localhost:2400/restaurants?mealId=5&stateId=2
 
 * List of all MealType
+> http://localhost:2400/mealType
 
 ///page 2
 * Restaurants on the basis of MealType
+> http://localhost:2400/restaurants?mealId=5
 
 * Filter on the basis of meal type + cuisine Type
+> http://localhost:2400/filter/3?cuisineId=1
 
 * Filter on the basis of meal type + high cost + low cost
 
--- zomatoapi/app.js
+++ zomatoapi/app.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-let mongo = require('mongo');
+let mongo = require('mongodb');
 let MongoClient = mongo.MongoClient;
 let dotenv = require('dotenv');
 dotenv.config()
@@ -9,15 +9,82 @@ let bodyParser = require('body-parser');
 let cors = require('cors');
 let port = process.env.PORT || 2300;
 let db;
+let authKey = process.env.authKey
 
 //middleware
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cors());
 
+
+function auth(key){
+    if(key === authKey){
+        return true
+    }else{
+        return false
+    }
+}
+
 //get heart beat
 app.get('/',(req,res) => {
     res.status(200).send('Health Ok')
+})
+
+///location
+app.get('/location',(req,res) => {
+    let key = req.header('x-basic-token')
+    if(auth(key)){
+        db.collection('location').find().toArray((err,data) => {
+            res.status(200).send(data)
+        })
+    }else{
+        res.status(201).send('Not Authentication Call')
+    }
+   
+})
+
+///restaurants
+app.get('/restaurants',(req,res) => {
+    let query = {};
+    let stateId = Number(req.query.stateId)
+    let mealId = Number(req.query.mealId)
+    if(stateId && mealId){
+        query = {state_id:stateId,'mealTypes.mealtype_id':mealId}
+    }else if(stateId){
+        query = {state_id:stateId}
+    }else if(mealId){
+        query={'mealTypes.mealtype_id':mealId}
+    }
+    db.collection('restaurants').find(query).toArray((err,data) => {
+        if(err) throw err
+        res.status(200).send(data)
+    })
+})
+
+///mealType
+app.get('/mealType',(req,res) => {
+    db.collection('mealType').find().toArray((err,data) => {
+        res.status(200).send(data)
+    })
+})
+
+
+///filters
+app.get('/filter/:mealId',(req,res) => {
+    let query = {}
+    let mealId = Number(req.params.mealId);
+    let cuisineId =  Number(req.query.cuisineId);
+    if(cuisineId){
+        query={
+            'mealTypes.mealtype_id':mealId,
+            'cuisines.cuisine_id':cuisineId
+        }
+    }
+
+    db.collection('restaurants').find(query).toArray((err,data) => {
+        if(err) throw err
+        res.status(200).send(data)
+    })
 })
 
 
